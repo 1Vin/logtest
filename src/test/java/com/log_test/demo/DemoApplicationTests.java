e66746a7d138a90f90a15bb2ce6a55aef2d9dfce
tom_hxc <779065985@qq.com>
2021-03-07 14:41:56 +0800

    2

---
+++ src/test/java/com/log_test/demo/DemoApplicationTests.java
@@ -0,0 +1,26 @@
+package com.log_test.demo;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class DemoApplicationTests {
+
+    Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Test
+    void contextLoads() {
+        System.out.println("这log日志");
+        logger.trace("这是trace日志");
+        logger.debug("这是debug日志");
+        logger.info("这是info日志");
+        logger.warn("这是warn日志");
+        logger.error("这是error日志1");
+    }
+
+}
